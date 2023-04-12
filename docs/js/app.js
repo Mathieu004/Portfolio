e3412806c940c4fe79a92c6f7714817e24dd2c67
Mathieu Cazaux <matcaz33.mobile@gmail.com>
2023-04-13 00:51:26 +0200

    overlay ajustements

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,10 +1,21 @@
 const overlay = document.querySelector('.overlay');
+const body = document.querySelector('body');
 
 window.onload = function() {
     overlay.classList.remove('hidden');
+    body.style.overflow = 'hidden';
     window.scrollTo(0, 0);
+
+    document.addEventListener('wheel', preventDefault, { passive: false });
     setTimeout(function() {
+        document.removeEventListener('wheel', preventDefault);
+        body.style.overflow = '';
         overlay.classList.add('hidden');
       }, 3000); 
 };
 
+function preventDefault(e) {
+    e.preventDefault();
+}
+
+/*----------------------------------------------------------  -------------------------------------------------------------*/
