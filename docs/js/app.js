934ae86c91c2c12dc321fa00c65dc2ffdacd9dd7
Mathieu Cazaux <matcaz33.mobile@gmail.com>
2023-04-27 00:32:34 +0200

    slide animation ok

--- docs/js/app.js
+++ docs/js/app.js
@@ -31,31 +31,56 @@ const flecheGauche = document.querySelector('.g');
 
 const box1 = document.querySelector('.box-swaping1');
 const box2 = document.querySelector('.box-swaping2');
+const box3 = document.querySelector('.box-swaping3');
 
 let numeroSlide = 1;
 
 flecheGauche.addEventListener("click", function() {
     if(numeroSlide === 2) {
-        box1.classList.add('animation1D');
-        box2.classList.add('animation2D');
-        box2.classList.remove('animation2G');
-        box1.classList.remove('animation1G');
+        box1.classList.add('animation1-1');
+        box2.classList.add('animation2-2');
+        box2.classList.remove('animation2-1G');
+        box2.classList.remove('animation2-1D');
+        box1.classList.remove('animation1-0');
         setTimeout(() => {
             box2.classList.add('none');
         }, 500);
         numeroSlide = 1;
+    }
+    else if(numeroSlide === 3) {
+      box2.classList.remove('none');
+      box2.classList.add('animation2-1G');
+      box3.classList.add('animation3-3');
+      box2.classList.remove('animation2-0');
+      box3.classList.remove('animation3-1');
+      setTimeout(() => {
+          box3.classList.add('none');
+      }, 500);
+      numeroSlide = 2;
     }
   });
 
 flecheDroite.addEventListener("click", function() {
     if(numeroSlide === 1) {
         box2.classList.remove('none');
-        box2.classList.add('animation2G');
-        box1.classList.add('animation1G');
-        box1.classList.remove('animation1D');
-        box2.classList.remove('animation2D');
+        box2.classList.add('animation2-1D');
+        box1.classList.add('animation1-0');
+        box1.classList.remove('animation1-1');
+        box2.classList.remove('animation2-2');
         numeroSlide = 2;
     }
+    else if(numeroSlide === 2) {
+      box3.classList.remove('none');
+      box3.classList.add('animation3-1');
+      box2.classList.add('animation2-0');
+      box2.classList.remove('animation2-1G');
+      box2.classList.remove('animation2-1D');
+      box3.classList.remove('animation3-3');
+      setTimeout(() => {
+        box2.classList.add('none');
+      }, 500);
+      numeroSlide = 3;
+  }
 })
 
 let lastClickedBox = null;
